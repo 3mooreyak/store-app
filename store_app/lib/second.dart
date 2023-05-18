b8d116e3f447970d47e5e7196d29aa41cd97af0d
Omar Khalil <omarkhalil@Omars-MacBook-Pro.local>
2023-05-18 14:03:40 +0300

    nav bar

--- store_app/lib/second.dart
+++ store_app/lib/second.dart
@@ -4,16 +4,23 @@ class Second extends StatelessWidget {
   var name;
   var image;
   var price;
-   Second({super.key, required this.name ,required this.image , required this.price});
+  var countt;
+   Second({super.key, required this.name ,required this.image , required this.price, required this.countt});
 
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
+      backgroundColor: Colors.amber,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(name ,style: TextStyle(fontSize: 40),),
-          Image.network(image),
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Image.network(image),
+          ),
+          Text('$price SAR',style: TextStyle(fontSize: 40),),
+          Text(countt == '0' ?"not found":'count is $countt ',style: TextStyle(fontSize: 40),)
         ],
       ),
     );
